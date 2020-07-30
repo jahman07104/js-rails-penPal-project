0452b6e904444ed37c9b368066f18634bcf3ac4d
Patrick Harrison <jahman07104@gmail.com>
2020-07-30 19:21:28 -0400

    code cleanup and added more images

--- api-backend/app/controllers/penpals_controller.rb
+++ api-backend/app/controllers/penpals_controller.rb
@@ -4,43 +4,12 @@ class PenpalsController < ApplicationController
     render json: penpals 
   end
 
-  def show
-    @penpal = Penpal.find(params[:id])
-    render json: penpals 
-  end
-
-  def new
-    @penpal = Penpal.new
-  end
-
-  def edit
-    @penpal = Penpal.find(params[:id])
-  end
-
   def create
     @penpal = Penpal.new(penpal_params)
   
-  if @penpal.save!
+    @penpal.save
     redirect_back fallback_location: ''
-  else 
-    redirect_to @penpal
-    #  render "new"
-    # redirect_to penpals_path
   end
-  end
-
-  def update
-    @penpal = Penpal.find(params[:id])
-    @penpal.update(penpal_params)
-
-    redirect_to penpal_path(@penpal)
-  end
-
-  private
-
-  def penpal_params
-    params.require(:penpal).permit(:name, :city, :email, :image)   
-  end  
 
   def destroy
     @penpal = Penpal.find(params[:id])
@@ -51,8 +20,6 @@ class PenpalsController < ApplicationController
       head(:unprocessable_entity)
 
       # head is how rails just the content in the header no content in the response..so this code says if everything went well with the destruction just return ok if not return the other thing and on the client side we can show alerts
-
-     redirect_to penpals_path
     end
   end
 
@@ -73,6 +40,14 @@ class PenpalsController < ApplicationController
     Penpal.where(user_id: user.id).update_all(user_id: nil)
   end
 
+
+  private
+
+  def penpal_params
+    params.require(:penpal).permit(:name, :city, :email, :image)   
+  end  
+
+  
  
 
 end
--- api-backend/app/models/penpal.rb
+++ api-backend/app/models/penpal.rb
@@ -2,10 +2,45 @@ class Penpal < ApplicationRecord
 belongs_to :user, optional: true
  validates :name, presence: true
 
-  def image=(image)
+  def image=(image)def destroy
+    @penpal = Penpal.find(params[:id])
+    # @penpal = Penpal.where([:id]).first
+    if @penpal.destroy
+      head(:ok)
+    else
+      head(:unprocessable_entity)
+
+      # head is how rails just the content in the header no content in the response..so this code says if everything went well with the destruction just return ok if not return the other thing and on the client side we can show alerts
+
+     redirect_to penpals_path
+    end
+  end
+
+  # POST /penpals/:penpal_id/choose
+  def choose
+    user = User.first
+    Penpal.find(params[:penpal_id]).update! user_id: user.id
+
+  end
+
+  # POST /penpals/:id/remove
+  def remove
+    Penpal.find(params[:penpal_id]).update! user_id: nil
+  end
+
+  def remove_all
+    user = User.first
+    Penpal.where(user_id: user.id).update_all(user_id: nil)
+  end
+
     image = image.tempfile if image.is_a?(ActionDispatch::Http::UploadedFile)
+    # generates random image file name, with original extension
     file = "#{SecureRandom.urlsafe_base64}#{File.extname(image)}"
     FileUtils.cp image, "public/images/#{file}"
     self.image_url = "/images/#{file}"
+  end
+
+
+  def image_url=(url)
   end
 end
